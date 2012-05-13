4e5b50ea9eb115183218251d1bb0cddf1dce5d28
emint <emint@emint-G53SX.(none)>
2012-05-12 22:56:08 -0700

    Adding new project for the shader processor. Also excluding the .settings configuration.

---
+++ shader_processor/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: May 12, 2012
+ *      Author: emint
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout<<"Hello world."<<endl;
+	return 1;
+}
