4ec28f8bd960e5924e3e49a0c50f11fa4a9d9f7c
emint <emint@emint-G53SX.(none)>
2012-06-01 12:41:27 -0700

    Adding dimensions to texture

--- engine/Texture.cpp
+++ engine/Texture.cpp
@@ -7,9 +7,15 @@
 
 #include "Texture.h"
 
-Texture::Texture(string path_) : path(path_) {
+Texture::Texture(string path_) :
+  path(path_) {
   img = new sf::Image();
   loadSuccesful = img->LoadFromFile(path);
+}
+
+Texture::Texture(unsigned int width_, unsigned int height_) :
+  path(""), loadSuccesful(true), width(width_), height(height_) {
+  img = new sf::Image(width, height);
 }
 
 Texture::~Texture() {
@@ -19,6 +25,18 @@ bool Texture::isLoaded() const {
   return loadSuccesful;
 }
 
-void Texture::bind()  const {
+void Texture::bind() const {
   img->Bind();
+}
+
+void Texture::setPixel(int x, int y, engine::Color color) {
+  img->SetPixel(x, y, sf::Color(color.r(), color.g(), color.b()));
+}
+
+unsigned int Texture::getHeight() const {
+  return height;
+}
+
+unsigned int Texture::getWidth() const {
+  return width;
 }
--- engine/Texture.h
+++ engine/Texture.h
@@ -16,11 +16,14 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
+#include "Color.h"
+
 using namespace std;
 
 class Texture {
   public:
     Texture(string path);
+    Texture(unsigned int width, unsigned int height);
     virtual ~Texture();
 
     //Whether or not the image was initialized correctly
@@ -28,6 +31,14 @@ class Texture {
 
     //Binds image using appropriate OpenGL calls
     void bind() const;
+
+    //Allows you to set an underlying pixel
+    void setPixel (int x, int y, engine::Color color);
+
+    //Gets dimensions
+    unsigned int getHeight() const;
+    unsigned int getWidth() const;
+
   private:
     //Path to image on disk
     string path;
@@ -37,6 +48,10 @@ class Texture {
 
     //Whether or not the image load was succesful
     bool loadSuccesful;
+
+    //Dimensions
+    unsigned int width;
+    unsigned int height;
 };
 
 #endif /* TEXTURE_H_ */
