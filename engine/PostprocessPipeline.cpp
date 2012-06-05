1522aa3a62f4aef3e4d07792059e1bbacfdb26bd
emint <emint@emint-G53SX.(none)>
2012-06-04 23:52:10 -0700

    Fixing bug where was not looping to end of effects

--- engine/PostprocessPipeline.cpp
+++ engine/PostprocessPipeline.cpp
@@ -9,6 +9,9 @@
 #include "PostprocessUtils.h"
 
 #include <stdio.h>
+#include <iostream>
+
+using namespace std;
 
 #define GL_CHECK(x) {\
   (x);\
@@ -33,16 +36,18 @@ void PostprocessPipeline::addEffect(PostprocessEffect* effect) {
 
 void PostprocessPipeline::captureBuffer() const {
   GL_CHECK(glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, effectsFBO));
+  PostprocessUtils::bindTexturesToBuffer(initRenderColor,
+      initRenderDepth, effectsFBO);
 }
 
 void PostprocessPipeline::releaseBuffer() const {
-  GL_CHECK(glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, effectsFBO));
+  GL_CHECK(glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0));
 }
 
 GLuint PostprocessPipeline::applyEffects() {
   GLuint prev = initRenderColor;
   for (vector<PostprocessEffect*>::iterator iter = effects.begin(); iter
-      != effects.begin(); ++iter) {
+      != effects.end(); ++iter) {
     PostprocessUtils::bindTexturesToBuffer((*iter)->getResultTextureHandle(),
         effectsDepth, effectsFBO);
     (*iter)->processEffect(initRenderColor, initRenderDepth, prev);
